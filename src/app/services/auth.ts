10a7a80159da10f19ce8bb8950b8732497a90c8f
SebasDavid14 <sebasdavid0211@gmail,com>
2026-09-18 19:35:34 -0500

    Configurar URL de backend en producción

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class Auth {
-  private apiUrl = '/api/auth';
+  private apiUrl = 'https://backend-production-8aec6.up.railway.app/api/auth';
 
   constructor(private http: HttpClient) {}
 
